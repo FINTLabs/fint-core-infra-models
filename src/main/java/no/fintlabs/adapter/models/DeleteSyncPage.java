e9176a212ad01d73e3c6d1d2ffb2cd6732aa25b3
Hknots <Hknots99@gmail.com>
2023-02-09 13:11:54 +0100

    Added DeleteSyncPage

---
+++ src/main/java/no/fintlabs/adapter/models/DeleteSyncPage.java
@@ -0,0 +1,9 @@
+package no.fintlabs.adapter.models;
+
+/**
+ * Represents a delete sync page.
+ * @param <T> The FINT resource type for the page.
+ */
+public class DeleteSyncPage<T> extends SyncPage<T> {
+
+}
