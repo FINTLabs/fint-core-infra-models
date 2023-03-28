91da26bbf199089231c166f075be5a9c8bda288f
Hknots <Hknots99@gmail.com>
2023-03-28 15:35:04 +0200

    add httpMethod to syncTypes

--- src/main/java/no/fintlabs/adapter/models/SyncType.java
+++ src/main/java/no/fintlabs/adapter/models/SyncType.java
@@ -1,7 +1,20 @@
 package no.fintlabs.adapter.models;
 
+import org.springframework.http.HttpMethod;
+
 public enum SyncType {
-    FULL,
-    DELTA,
-    DELETE
+    FULL(HttpMethod.POST),
+    DELTA(HttpMethod.PATCH),
+    DELETE(HttpMethod.DELETE);
+
+    private final HttpMethod httpMethod;
+
+    SyncType(HttpMethod httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+
+    public HttpMethod getHttpMethod() {
+        return httpMethod;
+    }
+
 }
