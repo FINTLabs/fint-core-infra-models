262be678431b57ca51e09b4d05564e4d7a83b5fe
Hknots <hknots99@gmail.com>
2024-08-31 01:53:06 +0200

    move SyncType to metadata & remove reduntant types of sync pages

--- src/main/java/no/fintlabs/adapter/models/sync/SyncPage.java
+++ src/main/java/no/fintlabs/adapter/models/sync/SyncPage.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class SyncPage {
+
     /**
      * Metadata describing the page.
      */
@@ -22,14 +23,5 @@ public class SyncPage {
      * Data in the page.
      */
     private List<SyncPageEntry> resources;
-
-    /**
-     * Type of sync.
-     */
-    private final SyncType syncType;
-
-    public SyncPage(SyncType syncType) {
-        this.syncType = syncType;
-    }
 
 }
--- src/main/java/no/fintlabs/adapter/models/sync/SyncPageMetadata.java
+++ src/main/java/no/fintlabs/adapter/models/sync/SyncPageMetadata.java
@@ -58,4 +58,21 @@ public class SyncPageMetadata {
      * @see System#currentTimeMillis()
      */
     private long time;
+
+    /**
+     * SyncType for the Syncpage.
+     *
+     * <p>This enum represents the different types of synchronization operations that can be performed.
+     * Each type is associated with a specific {@link org.springframework.http.HttpMethod HttpMethod} to indicate
+     * the type of HTTP request used for that operation.</p>
+     *
+     * <ul>
+     *     <li>{@link SyncType#FULL FULL}: Used for sending full datasets. This will send the entire set of data to the server using {@link org.springframework.http.HttpMethod#POST POST}.</li>
+     *     <li>{@link SyncType#DELTA DELTA}: Used for sending only updated or modified entries. This will send only the changes using {@link org.springframework.http.HttpMethod#PATCH PATCH}.</li>
+     *     <li>{@link SyncType#DELETE DELETE}: Used for deleting entities. This will remove the specified entities using {@link org.springframework.http.HttpMethod#DELETE DELETE}.</li>
+     * </ul>
+     *
+     */
+    private SyncType syncType;
+
 }
