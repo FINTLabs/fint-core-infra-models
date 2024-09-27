8ba49078976cc00dd1e125d0e35a5c051e2428d1
Hknots <hknots99@gmail.com>
2024-09-27 14:33:57 +0200

    implement FintResource

--- src/test/groovy/no/fintlabs/adapter/models/TestResource.java
+++ src/test/groovy/no/fintlabs/adapter/models/TestResource.java
@@ -1,16 +1,23 @@
 package no.fintlabs.adapter.models;
 
+import no.fint.model.FintIdentifikator;
 import no.fint.model.resource.FintLinks;
+import no.fint.model.resource.FintResource;
 import no.fint.model.resource.Link;
 
 import java.util.List;
 import java.util.Map;
 
-public class TestResource implements FintLinks {
+public class TestResource implements FintResource {
     private final Map<String, List<Link>> links = this.createLinks();
 
     @Override
     public Map<String, List<Link>> getLinks() {
         return links;
+    }
+
+    @Override
+    public Map<String, FintIdentifikator> getIdentifikators() {
+        return null;
     }
 }
