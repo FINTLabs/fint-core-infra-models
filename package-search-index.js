1de75ba3550bd785214ab6aefa3de62a15d53f81
hknots <hknots@users.noreply.github.com>
2024-10-29 08:37:04 +0000

    Deploying to javadocs from @ FINTLabs/fint-core-infra-models@776dead8f9f3acad0b9ffd36178ba812c12ff878 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"no.fintlabs.adapter.models"},{"l":"no.fintlabs.adapter.models.event"},{"l":"no.fintlabs.adapter.models.sync"},{"l":"no.fintlabs.adapter.models.utils"},{"l":"no.fintlabs.adapter.operation"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"no.fintlabs.adapter.models"},{"l":"no.fintlabs.adapter.models.utils"}]
