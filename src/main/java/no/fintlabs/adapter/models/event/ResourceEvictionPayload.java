4d5104e15595fbc59b40bff0b8adcd4d0c118a5e
Hknots <hknots99@gmail.com>
2025-03-19 11:25:03 +0100

    feat: add ResourceEvictionPayload

---
+++ src/main/java/no/fintlabs/adapter/models/event/ResourceEvictionPayload.java
@@ -0,0 +1,14 @@
+package no.fintlabs.adapter.models.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResourceEvictionPayload {
+    private final String domain;
+    private final String pkg;
+    private final String resource;
+    private final String org;
+
+}
